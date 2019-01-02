0625c81b908137c0cb4a292ef8e9b8b2b85d6c42
DaniTox <danitox@protonmail.com>
2019-01-02 01:34:09 +0100

    added TeenStarTable in CoreData

---
+++ SFA/CoreDataObjects/TeenStarTable.swift
@@ -0,0 +1,14 @@
+//
+//  TeenStarTable.swift
+//  SFA
+//
+//  Created by Dani Tox on 02/01/19.
+//  Copyright © 2019 Dani Tox. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class TeenStarTable: NSManagedObject {
+
+}
