ca2ab122f710e9355ea29a27eda21cbc7521398f
DaniTox <danitox@protonmail.com>
2019-01-21 17:11:08 +0100

    added sito core data object

---
+++ SFA/CoreDataObjects/SitoWeb+Relationships.swift
@@ -0,0 +1,18 @@
+//
+//  SitoWeb.swift
+//  SFA
+//
+//  Created by Dani Tox on 21/01/19.
+//  Copyright © 2019 Dani Tox. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class SitoWeb : NSManagedObject {
+    
+}
+
+class SitoWebCategoria : NSManagedObject {
+    
+}
